c24100e277f978ab117fcb73d00058d71f4c7ba7
Masudur Rahman Sourav <66933835+masudursourav@users.noreply.github.com>
2024-12-19 23:25:01 +0600

    pinia with vue project initialization

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="app">
+    <header>
+      <nav>
+        <RouterLink class="home" to="/">Home</RouterLink> |
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </header>
+
+    <RouterView />
+  </div>
+</template>
+<style>
+.app {
+  font-family: Arial, sans-serif;
+  text-align: center;
+  padding: 20px;
+}
+</style>
